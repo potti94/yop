f5e193699fb4044e95d992c0ac12aec735ebe752
potti94 <youssef.a.z@live.fr>
2017-08-31 05:03:16 +0100

    towards main

--- yop/cpu.py
+++ yop/cpu.py
@@ -7,7 +7,7 @@ from nodes import *
 from random import *
 import time
 from threading import Thread
-from Tkconstants import PAGES
+
 
 
 def set_nodes(nb_nodes):
@@ -26,47 +26,26 @@ def print_line(line,i,max_dashes,nodes,up_window,var,fix,n,page):
 	nb_dashes=int(((var)/fix)*max_dashes)
 	dashes="|"* nb_dashes
 	spaces=" "* int(max_dashes-nb_dashes)
-	line="%2s [%1s]:[%s%s]%5s%%||" % (i,(nodes[i].node_state[2])[3], dashes, spaces,mem_perc)
-	mvwaddstr(up_window,((i-(page-1)*20)+n)%10, ((i-(page-1)*20)//5)*len(line)+1, line,A_BOLD)
-
+	if n==0:		
+		line="%4s [%1s]:[%s%s]%5s%%" % (i,(nodes[i].node_state[0])[3], dashes, spaces,mem_perc)
+	else:
+		cover=" "
+		line="%4s [%1s]:[%s%s]%5s%%" % (cover,(nodes[i].node_state[0])[3], dashes, spaces,mem_perc)
+		
+	mvwaddstr(up_window,((i-page*20)*2+n)%10,((i-page*20)//5)*len(line)+1, line,A_BOLD)
 
 def print_total(middel_window,total_cap,total_used,choice):
 	perc=round(float((total_used/total_cap)*100),2)
 	nb_dashes=int(((total_used)/total_cap)*100)
 	dashes="|"*nb_dashes
-	spaces=" "* int(100-nb_dashes)
+	spaces=" "* int(60-nb_dashes)
 	if choice ==0:
 		line="MEM: [%s%s] %5s%%" % (dashes, spaces,perc)
 		mvwaddstr(middel_window,0, 0, line,A_BOLD)
 	else:
 		line="CPU: [%s%s] %5s%%" % (dashes, spaces,perc)
 		mvwaddstr(middel_window,1, 0, line,A_BOLD)
-			
-	
-
-
-"""def controle_display(page,nb_pages,temp):
-	if page!=1 and page!=nb_pages:
-		if temp==KEY_UP:
-			page-=1
-			return page*4,page*4+16
-		elif temp==KEY_DOWN:
-			page+=1
-			return page*4,page*4+16
-	elif page==1:
-		if temp==KEY_DOWN:
-			page+=1
-			return page*4,page*4+16
-	elif page==nb_pages:
-		if temp==KEY_UP:
-			page-=1
-			return page*4,page*4+16"""
 				
-						
-			
-
-
-
 
 
 
@@ -82,7 +61,7 @@ def header(nb_nodes,nodes,up_window,middel_window,page):
 	choice=0
 	start=0
 	finish=20
-	
+	global nb_pages
 	
 	nb_pages=nb_nodes//20
 	#============ memory ==================
@@ -107,26 +86,11 @@ def header(nb_nodes,nodes,up_window,middel_window,page):
 	choice=1
 	print_total(middel_window,total_cap,total_used,choice)
 #==============================================================================		
-		
-	if page!=1 and page!=nb_pages:
-		if temp==KEY_UP:			
-			page-=1
-			start=(page-1)*20
-			finish=(page-1)*20+20
-		elif temp==KEY_DOWN:
-			page+=1
-			start=(page-1)*20
-			finish=(page-1)*20+20
-	elif page==1:
-		if temp==KEY_DOWN:
-			page+=1
-			start=(page-1)*20
-			finish=(page-1)*20+20
-	elif page==nb_pages:
-		if temp==KEY_UP:
-			page-=1
-			start=(page-1)*20
-			finish=(page-1)*20+20
+	if page >nb_pages:
+		page=nb_pages
+			
+	start=page*20
+	finish=page*20+20
 	i=start
 	f=finish
 	while i<f and i<nb_nodes:
@@ -135,7 +99,6 @@ def header(nb_nodes,nodes,up_window,middel_window,page):
 		j+=nodes[i].capability[0]
 		k+=nodes[i].used[0]
 		i+=1
-		n+=1
 	i=start
 	n=1		
 	while i<f and i<nb_nodes:
@@ -143,25 +106,116 @@ def header(nb_nodes,nodes,up_window,middel_window,page):
 		print_line(line,i,max_dashes,nodes,up_window,nodes[i].used[1],nodes[i].capability[1],n,page)
 		j+=nodes[i].capability[1]
 		k+=nodes[i].used[1]
-		i+=1
-		n+=1
-		
+		i+=1	
 	total_cap=j-1
 	total_used=k		
- 	#print_total(middel_window,total_cap,total_used)
-
 	total_cap=j
 	total_used=k	
 	
 def footer(down_window,y):
 	init_pair(1,COLOR_WHITE,COLOR_BLUE)
 	long=""
-	for line in {("M :","MEMORY "),("DOWN :","CHANGE NODES "),("UP :","CHANGE NODES "),("I :","ID "),("Q :","QUIT")}:
+	for line in {("h :","HELP "),("DOWN :","CHANGE NODES "),("UP :","CHANGE NODES "),("n :","NODES "),("q :","QUIT")}:
 		mvwaddstr(down_window,y-13,len(long)+1, line[0],A_BOLD)
 		long=long+line[0]
 		mvwaddstr(down_window,y-13,len(long)+1, line[1],color_pair(1)+A_REVERSE+A_BOLD)
 		long=long+line[1]
 		
+def limits(down_window,decal,y):
+	i=1
+	init_pair(3,COLOR_WHITE,COLOR_BLUE)
+	while i<17:
+		mvwaddstr(down_window,0,i*int(decal)-1,"|",color_pair(3))
+		j=1
+		while j<y-13:
+			mvwaddstr(down_window,j,i*int(decal)-1,"|")
+			j+=1
+		i+=1	
+	
+
+def corp(down_window,nodes,nb_nodes,page,x,y):
+	decal=round(x/17)
+	i=j=0
+	init_pair(2,COLOR_BLACK,COLOR_BLUE)
+	long=""
+	for line in {"Node","User","Queue","S","ID","UMEM","RMEM","NMEM","UC","RC","NC","UCO","RCO","T","P","Time+"}:
+		long+=line+" "*int((decal-len(line)))
+	long+="Name"+" "*int(x-len(long)-4)
+	mvwaddstr(down_window,0,0,long,color_pair(2)+A_BLINK)
+	limits(down_window,decal,y)
+	nb_pages=nb_nodes//20
+	if page >nb_pages:
+		page=nb_pages
+			
+	start=page*20
+	finish=page*20+20
+	i=start
+	f=finish
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].user[0], A_BOLD)
+		i+=1
+	j+=1
+	i=start
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].queue[0], A_BOLD)
+		i+=1
+	i=start	
+	j+=1
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), (nodes[i].node_state[0])[3], A_BOLD)
+		i+=1
+	j+=1
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal),i, A_BOLD)
+		i+=1
+	j+=1
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].mem[0], A_BOLD)
+		mvwaddstr(down_window, i%20+1, (j+1)*int(decal), nodes[i].mem[1], A_BOLD)
+		mvwaddstr(down_window, i%20+1, (j+2)*int(decal), nodes[i].mem[2], A_BOLD)
+		i+=1
+	j+=3
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].c[0], A_BOLD)
+		mvwaddstr(down_window, i%20+1, (j+1)*int(decal), nodes[i].c[1], A_BOLD)
+		mvwaddstr(down_window, i%20+1, (j+2)*int(decal), nodes[i].c[2], A_BOLD)
+		i+=1
+	j+=3
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].co[0], A_BOLD)
+		mvwaddstr(down_window, i%20+1, (j+1)*int(decal), nodes[i].co[1], A_BOLD)
+		i+=1
+	j+=2
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].t[0], A_BOLD)
+		i+=1
+	j+=1
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), str(nodes[i].p[0])+"%", A_BOLD)
+		i+=1
+	j+=1
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].time[0], A_BOLD)
+		i+=1
+	j+=1
+	i=start	
+	while i<f and i<nb_nodes:
+		mvwaddstr(down_window, i%20+1, j*int(decal), nodes[i].name[0], A_BOLD)
+		i+=1
+	j+=1
+	
+
+
+
+
+
 		
 
 class display(Thread):
@@ -173,15 +227,13 @@ class display(Thread):
         start_color()
         keypad(stdscr,True)
         noecho()
-        curs_set(False)   
-        page=1
-        x=stdscr.getmaxyx()[1]
-        y=stdscr.getmaxyx()[0]
-    
+        curs_set(False)
         while key!=ord("q"):
             #nodes=get_cluster_nodes()
-            #nb_nodes=randint(0,36)
-            nb_nodes=84
+            #nb_nodes=randint(0,365)
+            x=stdscr.getmaxyx()[1]
+            y=stdscr.getmaxyx()[0]
+            nb_nodes=210
             nodes=set_nodes(nb_nodes)
         #     ================== window decomposition ===========================
             up_window=newwin(10,x, 0, 0)
@@ -194,21 +246,38 @@ class display(Thread):
         #   ===================================================================               
             header(nb_nodes,nodes,up_window,middel_window,page)
             footer(down_window,y)
+            corp(down_window,nodes,nb_nodes,page,x,y)
             update_panels()
             doupdate()
-            temp=109
-            time.sleep(2)			
+            time.sleep(1)			
 
 def main():
-	global key
 	global temp
-	key=temp=109
-	thread1=display(key)
+	global key
+	global page
+	page=0
+	temp=key=109
+	
+	thread1=display(temp)
 	thread1.start()
 	while key!=ord("q"):
+		temp=109
 		key=getch()
-		if key==ord("m") or key==KEY_UP or key==KEY_DOWN or key==ord("v"):
+		if key==ord("h") or key==KEY_UP or key==KEY_DOWN or key==ord("n"):
 			temp=key
+			if temp==KEY_UP or temp==KEY_DOWN:
+				if page!=0 and page!=nb_pages:
+					if temp==KEY_UP:			
+						page-=1
+					if temp==KEY_DOWN:
+						page+=1
+				elif page==0:
+					if temp==KEY_DOWN:
+						page+=1
+				elif page==nb_pages:
+					if temp==KEY_UP:
+						page-=1
+			
 	
 	thread1.join()		
 	endwin()
--- yop/nodes.py
+++ yop/nodes.py
@@ -6,11 +6,19 @@ def randomize(a):
 
 class Nodes:
 	def __init__(self):
-		self.node_id=["dn04.hadoop.suty-ops.com",0000]                    #get_node_id(self)      #host/port
+		self.node_id=[1502084,0000]                    #get_node_id(self)      #host/port
 		self.http_rack=["dn04.hadoop.suty-ops.com:8042","/default"]       #get_http_rack(self)    #returns a list of http addr and rack name 
 		self.used=[1,1]                                                   #get_used(self)		 #used memory/ used cores	
 		self.capability=[randint(1,8000),randint(1,10)]                   #get_capability(self)	 #MEMORY CAPABILITY/ number of cores	
-		self.node_state=["",1502059470684,"NS_RUNNING"]                   #get_node_state(self)   #health report/ last health report time/ STATE
-																												#NS: NS_RUNNING
+		self.node_state=["NS_RUNNING"]                   #get_node_state(self)   #health report/ last health report time/ STATE
+		self.user=["Yassine.Azzouz"]												#NS: NS_RUNNING
+		self.queue=[1]
+		self.mem=[9600,12000,17000]	                             #mem=[UMEM,RMEM,NMEM]
+		self.c=[5,7,2]                                          # c=[UC,RC,NC]
+		self.co=[7,10]                                          # co=[UCO,RCO]
+		self.t=["MAPREDUCE"]
+		self.p=[42]
+		self.time=["8:19:32"]
+		self.name=["yoping.py"]
 		
 							
